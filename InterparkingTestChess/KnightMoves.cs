cc8230a41ffb85e122b55bdc5a28c0512f06bf34
Tuan-Tu Tran <tuan-tu.tran@satellit.be>
2020-10-27 09:53:50 +0100

    implement

--- InterparkingTestChess/KnightMoves.cs
+++ InterparkingTestChess/KnightMoves.cs
@@ -8,7 +8,31 @@ namespace InterparkingTestChess
         public static List<(int row, int col)> GetAllowDestinations(int row, int col)
         {
             var allowedDestinations = new List<(int, int)>();
+            var deltas = new List<(int, int)> { (2, 1), (1, 2) };
+            var signs = new List<int> { -1, 1 };
+
+            foreach (var (deltaRow, deltaColumn) in deltas)
+            {
+                foreach (var signDeltaRow in signs)
+                {
+                    foreach (var signDeltaColumn in signs)
+                    {
+                        var dstRow = row + signDeltaRow * deltaRow;
+                        var dstCol = col + signDeltaColumn * deltaColumn;
+                        if(IsInRange(dstRow) && IsInRange(dstCol))
+                        {
+                            allowedDestinations.Add((dstRow, dstCol));
+                        }
+                    }
+                }
+            }
             return allowedDestinations;
         }
+
+        private static bool IsInRange(int x)
+        {
+            return x >= 0 && x <= 7;
+        }
+
     }
 }
